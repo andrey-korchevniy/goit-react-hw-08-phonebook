c19efffd769f6ffa27bf53b0e7c4449f33320a51
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-09-05 11:23:08 +0300

    bugs

--- src/components/UI/AddNewContactButton/AddNewContactButton.styled.js
+++ src/components/UI/AddNewContactButton/AddNewContactButton.styled.js
@@ -1,39 +1,40 @@
-import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const Title = styled.span`
-    font-size: 14px;
-    font-weight: 500;
-    color: #3c4043;
-    margin-left: 15px;
-    letter-spacing: 0.25px;
-`
+  font-size: 14px;
+  font-weight: 500;
+  color: #3c4043;
+  margin-left: 15px;
+  letter-spacing: 0.25px;
+`;
 export const AddContactBtn = styled(NavLink)`
-    display: inline-flex;
-    margin: 23px 0;
-    border: 0;
-    background-color: #fff;
-    align-items: center;
-    border-radius: 24px;
-    box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%), 0 1px 3px 1px rgb(60 64 67 / 15%);
-    box-sizing: border-box;
-    cursor: pointer;
-    height: 48px;
-    justify-content: center;
-    padding: 2px 24px;
-    transition: box-shadow 280ms cubic-bezier(0.4,0,0.2,1),opacity 15ms linear 30ms,transform 270ms 0ms cubic-bezier(0,0,0.2,1);
-    transition-property: box-shadow, background-color, transform;
-    transition-duration: 24ms, 100ms, 270ms;
-    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-    transition-delay: 0s, 30ms, 0ms;
-    
-    :hover {
-        background-color: #F6FAFE;
-    }
+  display: inline-flex;
+  margin: 23px 0;
+  border: 0;
+  background-color: #fff;
+  align-items: center;
+  border-radius: 24px;
+  box-shadow: 0 1px 2px 0 rgb(60 64 67 / 30%), 0 1px 3px 1px rgb(60 64 67 / 15%);
+  box-sizing: border-box;
+  cursor: pointer;
+  height: 48px;
+  justify-content: center;
+  padding: 2px 24px;
+  transition: box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1),
+    opacity 15ms linear 30ms, transform 270ms 0ms cubic-bezier(0, 0, 0.2, 1);
+  /* transition-property: box-shadow, background-color, transform; */
+  transition-duration: 24ms, 100ms, 270ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-delay: 0s, 30ms, 0ms;
 
-    :active {
-        border-color: rgb(66, 133, 244);
-        border-width: 2px;
-        border-style: solid;
-    }
-`
+  :hover {
+    background-color: #f6fafe;
+  }
+
+  :active {
+    border-color: rgb(66, 133, 244);
+    border-width: 2px;
+    border-style: solid;
+  }
+`;
