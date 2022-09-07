dec84c3f7af6fb4e51dd18703037137988b0d8a8
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-09-07 21:36:10 +0300

    bug fix

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Container, Main } from './Layout.styled';
 import { NavMenu } from './NavMenu/NavMenu';
-import { Suspense } from 'react';
-import { Spinner } from 'react-bootstrap';
 
 export default function Layout() {
   return (
